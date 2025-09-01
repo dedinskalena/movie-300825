ba332f6047751a457faa4b86238864e88587de2e
dedinskalena <35485425+dedinskalena@users.noreply.github.com>
2025-09-01 17:06:24 +0300

    render home page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 const express=require('express');
- 
+const handlebars=require('express-handlebars')
+const app=express()
+const port=5000
+const path=require('path')
+
+app.engine('hbs',handlebars.engine({
+    extname:'hbs'
+}))
+app.set('view engine','hbs')
+app.set('views',path.join(__dirname,'views'))
+app.use(express.static('src/public'))
+
+app.get('/',(req,res)=>{
+    res.render('home',{layout:false})
+})
+app.listen(port,()=>console.log(`server is listening on port ${port} `))
