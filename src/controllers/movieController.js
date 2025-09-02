6740a03f4393817bce9faee52f41f48bd30201ff
dedinskalena <35485425+dedinskalena@users.noreply.github.com>
2025-09-02 16:57:53 +0300

    add body parser and movie serviece

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,9 +1,15 @@
 const router=require('express').Router()
+const  movieService=require('../services/movieService')
 
 router.get('/create',(req,res)=>{
     res.render('create')
 })
  
+router.post('/create',(req,res)=>{
+    //console.log(req.body)
+    const newMovie=req.body
+    movieService.create(newMovie)
 
+})
 
 module.exports=router
