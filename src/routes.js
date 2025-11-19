d7bf5529bd46f77d7162b340d3165e8d0ccd0569
dedinskalena <35485425+dedinskalena@users.noreply.github.com>
2025-11-19 11:00:01 +0200

    add register page

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const router=require('express').Router()
 const homeController=require('./controllers/homeController')
 const movieController=require('./controllers/movieController')
 const castController=require('./controllers/castControler')
+const authControler=require('./controllers/authControler')
 
 router.use(homeController)
 router.use(movieController)
 router.use('/cast',castController)
+router.use('/auth',authControler)
 
 
 router.get(/(.*)/,(req,res)=>{
